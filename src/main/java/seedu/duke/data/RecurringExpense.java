8ea042b518c755a4bca9bcbc352df2665aa2f18a
Kynaston <weeyuqikynaston@gmail.com>
2026-03-24 17:15:39 +0800

    Add RecurringExpense class skeleton

---
+++ src/main/java/seedu/duke/data/RecurringExpense.java
@@ -0,0 +1,13 @@
+package seedu.duke.data;
+
+import java.math.BigDecimal;
+import seedu.duke.category.Category;
+/**
+ * Represents a recurring expense template.
+ */
+public class RecurringExpense {
+
+    private final String name;
+    private final BigDecimal amount;
+    private final Category category;
+}
