ce652e1fcee1ce1ee8389a8d272fca4fb7abbcb6
Kynaston <weeyuqikynaston@gmail.com>
2026-03-24 18:06:39 +0800

    Added RecurringExpense Applicatin Logic

--- src/main/java/seedu/duke/data/RecurringExpenseList.java
+++ src/main/java/seedu/duke/data/RecurringExpenseList.java
@@ -31,4 +31,17 @@ public class RecurringExpenseList {
     public boolean isValidIndex(int inputIndex) {
         return inputIndex >= 1 && inputIndex <= recurringExpenses.size();
     }
+    public void applyTo(ExpenseList expenseList) {
+        assert expenseList != null : "Expense list must not be null";
+
+        for (RecurringExpense recurringExpense : recurringExpenses) {
+            assert recurringExpense != null : "Recurring expense in list must not be null";
+
+            expenseList.add(
+                    recurringExpense.getName(),
+                    recurringExpense.getAmount(),
+                    recurringExpense.getCategory()
+            );
+        }
+    }
 }
